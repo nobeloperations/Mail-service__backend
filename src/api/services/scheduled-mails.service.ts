554c470336256a14805fd4cc6b602eb14fad4922
Nikita Kutsokon <nikitakutsokon@gmail.com>
2023-11-20 13:18:04 +0200

    refactor: change tables names

--- src/api/services/scheduled-mails.service.ts
+++ src/api/services/scheduled-mails.service.ts
@@ -1,32 +1,31 @@
 import { Prisma } from '@prisma/client';
 import prismaClient from '../../database/prisma-client';
 
-const createMails = async (mailsData: Prisma.ScheduledMailsCreateManyInput) => {
-    const result = await prismaClient.scheduledMails.createMany({ data: mailsData });
+const createMails = async (mailsData: Prisma.ScheduledMailCreateManyInput) => {
+    const result = await prismaClient.scheduledMail.createMany({ data: mailsData });
     return result;
 };
 
 const getMailById = async (id: string) => {
-    const result = await prismaClient.scheduledMails.findUnique({ where: { id } });
+    const result = await prismaClient.scheduledMail.findUnique({ where: { id } });
     return result;
 };
 
 const deleteMailById = async (id: string) => {
-    const result = await prismaClient.scheduledMails.delete({ where: { id } });
+    const result = await prismaClient.scheduledMail.delete({ where: { id } });
     return result;
 };
 
-const updateMailById = async (id: string, mailData: Prisma.ScheduledMailsUpdateInput) => {
-    const result = await prismaClient.scheduledMails.update({ where: { id }, data: mailData });
+const updateMailById = async (id: string, mailData: Prisma.ScheduledMailUpdateInput) => {
+    const result = await prismaClient.scheduledMail.update({ where: { id }, data: mailData });
     return result;
 };
 
 const getMailsList = async () => {
-    const result = await prismaClient.scheduledMails.findMany();
+    const result = await prismaClient.scheduledMail.findMany();
     return result;
 };
 
-const getPendingMailsList = async () => {};
 
 export default {
     createMails,
@@ -34,5 +33,4 @@ export default {
     deleteMailById,
     updateMailById,
     getMailsList,
-    getPendingMailsList
 };
