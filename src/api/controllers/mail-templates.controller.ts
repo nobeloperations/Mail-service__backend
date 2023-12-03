e7a4fdbc51baafda995568d33cf9fe131a853323
Nikita Kutsokon <nikitakutsokon@gmail.com>
2023-12-03 17:05:24 +0200

    feat: Implement API for interacting with mail templates

---
+++ src/api/controllers/mail-templates.controller.ts
@@ -0,0 +1,44 @@
+import { Request, Response } from 'express';
+import { StatusCodes, ReasonPhrases } from 'http-status-codes';
+
+import MailTemplatesService from '../services/mail-templates.service';
+
+const getMailTemplateDataById = async (req: Request, res: Response) => {
+    const id = req.params.id;
+    const templateData = await MailTemplatesService.getMailTemplateDataById(id);
+
+    res.status(StatusCodes.OK).send(templateData);
+};
+
+const getMailTemplatesList = async (req: Request, res: Response) => {
+    const { page, pageSize } = req.query;
+    const result = await MailTemplatesService.getMailTemplatesList({ 
+        page: Number(page) || 1, 
+        pageSize: Number(pageSize) || 10
+    });    
+
+    res.status(StatusCodes.OK).json(result);
+};
+
+const deleteMailTemplateById = async (req: Request, res: Response) => {
+    const id = req.params.id;
+    const result = await MailTemplatesService.deleteMailTemplateById(id);
+
+    res.status(StatusCodes.NO_CONTENT).json(result);
+};
+
+const createMailTemplates = async (req: Request, res: Response) => {
+    if (!req.files) {
+        return res.status(StatusCodes.BAD_REQUEST).send(ReasonPhrases.BAD_REQUEST);
+    }
+
+    await MailTemplatesService.createMailTemplates(req.files)
+    res.status(StatusCodes.CREATED).send(ReasonPhrases.CREATED)
+};
+
+export default {
+    createMailTemplates,
+    getMailTemplatesList,
+    deleteMailTemplateById,
+    getMailTemplateDataById,
+};
