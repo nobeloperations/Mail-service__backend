0d3cc5a16aadf15f550d83170433a558d011dbf7
Nikita Kutsokon <nikitakutsokon@gmail.com>
2023-12-07 09:56:38 +0200

    refactor: refactor code that responsible of sending pending mails

--- src/cron-jobs/jobs/sent-pending-mails.ts
+++ src/cron-jobs/jobs/sent-pending-mails.ts
@@ -1,52 +1,27 @@
-const nodemailer = require('nodemailer');
-require("dotenv").config()
 
-import ScheduledMailsService from '../services/scheduled-mails.service';
 import ContactDataService from '../services/contactData'
 import SendedMailsService from '../services/sended-mails'
-import { replacePlaceholdersWithContactDataInMailTemplate } from "../../mails-processor/mail-placeholder-replacer"
-import { Contact } from '@prisma/client';
+import ScheduledMailsService from '../services/scheduled-mails.service';
 
-const {GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_CLIENT_REFRESH_TOKEN} = process.env
+import MailSender from '../../infrustructure/services/mail/mail-sender.service';
+import MailComposer from '../../infrustructure/services/mail/mail-composer.service';
 
 
-const config = {
-    service: 'gmail',
-    auth: {
-        type: 'OAuth2',
-        user: 'nikita.k@nobelcoaching.com',
-        clientId: GOOGLE_CLIENT_ID,
-        clientSecret: GOOGLE_CLIENT_SECRET,
-        refreshToken: GOOGLE_CLIENT_REFRESH_TOKEN,
-    }
-}
-const transporter = nodemailer.createTransport(config);
-  
 const sentPendingMails = async () => {
     const pendingMails = await ScheduledMailsService.retrievePendingMails();
-    pendingMails.map(async ({contactId, id, ...restOfFields}) => {
-        const contactData = await ContactDataService.retrieveContactData(contactId)
-        const proccessedMessage = await replacePlaceholdersWithContactDataInMailTemplate(`<html><body>
-        <p>Hi, I test everything</p>
-        <img src="http://localhost:3000/tracking?emailId=${id}" width="1" height="1" />
-        </body></html>`, contactData)
 
-        transporter
-        .sendMail(setAppropriateMailOptions(contactData, proccessedMessage))
-        .then(() => {
-            ScheduledMailsService.deletePendingMail(id)
-            SendedMailsService.addSendedMail({...restOfFields, contactId})
-        })
-        .catch(err => console.log(err.message));
-    }) 
+    //TODO: Handle an error if it was not possible to send a mail
+    for (const processedSheduledMailData of pendingMails) {
+        const { contactId, id, templateId, ...scheduledMailData } = processedSheduledMailData;
+
+        const contactData = await ContactDataService.retrieveContactData(contactId);
+        const composedMail = await MailComposer.composeMail(contactData, templateId);
+
+        await MailSender.sentComposedMail(contactData.email, composedMail);
+
+        await ScheduledMailsService.deletePendingMail(id);
+        await SendedMailsService.addSendedMail(processedSheduledMailData);
+    }
 };
-
-function setAppropriateMailOptions(contactData: Contact, html: string){
-    return {
-        to: contactData.email,
-        subject: 'Nodemailer test',
-        html,
-      };
-}
 
 export default sentPendingMails;
--- src/cron-jobs/services/sended-mails.ts
+++ src/cron-jobs/services/sended-mails.ts
@@ -1,7 +1,7 @@
 import { ScheduledMail } from '@prisma/client';
 import prismaClient from '../../database/prisma-client';
 
-const addSendedMail = async (mailData) => {
+const addSendedMail = async (mailData: ScheduledMail) => {
     const sendedmMail = await prismaClient.sentMail.create({
         data: {
             ...mailData
