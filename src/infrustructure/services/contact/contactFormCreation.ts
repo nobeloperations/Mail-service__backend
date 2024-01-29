075a4114ab6dc17fd5346a4960ff1581b8a95b78
Nikita Kutsokon <nikitakutsokon@gmail.com>
2024-01-29 16:54:26 +0200

    refactor: made the logic of the contact location definition inside infrustructure folder

--- src/infrustructure/services/contact/contactFormCreation.ts
+++ src/infrustructure/services/contact/contactFormCreation.ts
@@ -2,9 +2,9 @@ import { Router, Response, Request } from 'express';
 import ContactService from '../../../api/services/contacts.service';
 import validateBody from '../../../api/middlewares/request-body-validator';
 import contactsSchema from '../../../api/request-schemas/contacts.request-schemas';
-import { getLocationByIpAddress } from '../../../user-actions-system/services/contactLocation.service';
 import { StatusCodes } from 'http-status-codes';
-import IP from 'ip';
+
+import getContactLocationByIpAddress from '../../detecting-contacts-location/user-location-detector.service';
 
 const router = Router()
 
@@ -12,10 +12,8 @@ router.post("/", validateBody(contactsSchema.createResourseFormSubmitionForm), a
     try {
         const contactData = req.body;
         const userIpAddress = ((req.headers['x-forwarded-for'] as string) || '').split(',')[0].trim() || req.socket.remoteAddress;
-        console.log(`Controller ip address x-forwarded-for: ${req.headers['x-forwarded-for']}`);
-        console.log(`Controller ip address remoteAddress: ${req.socket.remoteAddress}`);
     
-        const userLocation = await getLocationByIpAddress(userIpAddress);
+        const userLocation = await getContactLocationByIpAddress(userIpAddress);
         
         if (userLocation && (userLocation.country === 'Russia' || userLocation.country === 'Belarus')) {
             return res.status(StatusCodes.FORBIDDEN).json('It is not possible to create a contact from Russia or Belarus').end();
